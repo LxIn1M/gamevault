3c07e9668e9e5c587cfb67c292b123ac423538d9
LxIn1M <michalminial@gmail.com>
2026-09-11 01:36:41 +0200

    Refactor library templates and game management.

--- config/urls.py
+++ config/urls.py
@@ -2,14 +2,13 @@ from core import views
 from django.contrib import admin
 from django.urls import path, include
 
-from core.views import home, register, add_game, edit_entry, delete_entry, search_games, add_rawg_game
+from core.views import home, register, edit_entry, delete_entry, search_games, add_rawg_game
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
     path("", home, name="home"),
     path("register/", register, name="register"),
-    path("add-game/", add_game, name="add_game"),
     path("entry/<int:entry_id>/edit/", edit_entry, name="edit_entry"),
     path("entry/<int:entry_id>/delete/", delete_entry, name="delete_entry"),
     path("search/", search_games, name="search_games"),
--- core/views.py
+++ core/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from .models import LibraryEntry, Game
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect
-from .forms import LibraryEntryForm, LibraryEntryUpdateForm
+from django.shortcuts import redirect, get_object_or_404
+from .forms import LibraryEntryUpdateForm
 import os
 import requests
 from dotenv import load_dotenv
@@ -12,7 +12,9 @@ load_dotenv()
 
 @login_required
 def home(request):
-    entries = LibraryEntry.objects.filter(user=request.user)
+    entries = LibraryEntry.objects.filter(
+        user=request.user
+    ).select_related("game")
 
     context = {
         "username": request.user.username,
@@ -36,27 +38,12 @@ def register(request):
     return render(request, "registration/register.html", {"form": form})
 
 @login_required
-def add_game(request):
-    if request.method == "POST":
-        form = LibraryEntryForm(request.POST)
-
-        if form.is_valid():
-            entry = form.save(commit=False)
-            entry.user = request.user
-            entry.save()
-
-            return redirect("home")
-    else:
-        form = LibraryEntryForm()
-
-    return render(request, "core/add_game.html", {"form": form})
-
-@login_required
 def edit_entry(request, entry_id):
-    entry = LibraryEntry.objects.get(
-        id=entry_id,
-        user=request.user,
-    )
+    entry = get_object_or_404(
+    LibraryEntry,
+    id=entry_id,
+    user=request.user,
+)
 
     if request.method == "POST":
         form = LibraryEntryUpdateForm(
@@ -82,10 +69,11 @@ def edit_entry(request, entry_id):
     
 @login_required
 def delete_entry(request, entry_id):
-    entry = LibraryEntry.objects.get(
-        id=entry_id,
-        user=request.user,
-    )
+    entry = get_object_or_404(
+    LibraryEntry,
+    id=entry_id,
+    user=request.user,
+)
 
     if request.method == "POST":
         entry.delete()
